df26acefb16be0ba73eae43f8374d157c955366b
Fabio R <fabioruscica@hotmail.it>
2020-12-27 17:38:40 +0100

    Prima iterazione della classe TrainTime che gestisce le timetable

---
+++ ldp_Assegnamento2/TrainTime.cpp
@@ -0,0 +1,23 @@
+/*
+    Fabio Ruscica
+*/
+
+#include "TrainTime.h"
+
+bool TrainTime::register_timetable(std::string file_name)
+{
+    //TODO
+    return true;
+}
+
+TrainInfo TrainTime::get_train_info(int train_number) const
+{
+    // TODO
+    return TrainInfo();
+}
+
+bool TrainTime::is_valid_time(const int time) const
+{
+    // TODO
+    return true;
+}
---
+++ ldp_Assegnamento2/TrainTime.h
@@ -0,0 +1,24 @@
+/*
+	Fabio Ruscica
+*/
+
+#ifndef traintime_h
+#define traintime_h
+
+#include <map>
+#include <string>
+#include <vector>
+#include "generics.h"
+
+class TrainTime
+{
+public:
+	bool register_timetable(std::string file_name);
+	TrainInfo get_train_info(int train_number) const;
+private:
+	std::map<int, TrainInfo> m_timetable;
+	bool is_valid_time(const int time) const;
+};
+
+#endif
+
