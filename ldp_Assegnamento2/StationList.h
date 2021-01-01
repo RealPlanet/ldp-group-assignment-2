418a2a68816f830407d4c936c8d56757d2803a4d
Vice <matteo.salvalaio@studenti.unipd.it>
2021-01-01 18:50:08 +0100

    Finita la bozza di progetto

--- ldp_Assegnamento2/StationList.h
+++ ldp_Assegnamento2/StationList.h
@@ -3,82 +3,47 @@
 #ifndef STATIONLIST_H
 #define STATIONLIST_H
 
-#include <vector>
+/************************************************
+ *  La classe si occupa di definire i metodi per la
+ *  creazione di una lista con posizioni astratte
+ *  (Abstract Positional List).
+ *  Alcuni metodi come quelli per la rimozioni di
+ *  oggetti dalla lista non sono stati implementati
+ *  in quanto non di utilità per il programma complessivo.
+ *  Il costruttore di copia e l'assegnamento di copia
+ *  sono stati disabilitati in quanto se lasciati
+ *  default avrebbero prodotto lo stesso oggetto e non
+ *  una copia e l'effettiva formazione di una copia di
+ *  questa classe non è necessaria nel progetto.
+ ***********************************************/
+
 #include "Station.h"
 
-//Invariants
-// - size >= 0
-
 class StationList {
-private:
-
-	class StationNode {
-	
-	public:
-		explicit StationNode(Station* s = new Station())			//Default Constructor
-		: next{nullptr}, prev{nullptr}, station{s} {}
-		
-		~StationNode() {delete station;}							//Destructor
-		
-		StationNode(const StationNode& node) = delete;				//Copy Constructor
-		StationNode(StationNode&& node) = delete;					//Move Constructor
-		StationNode& operator=(const StationNode& node) = delete;	//Copy Assignment
-		StationNode& operator=(StationNode&& node) = delete;		//Move Assignment
-
-		void setNext(StationNode* node) {next = node;}				//Set the next node
-		void setPrev(StationNode* node) {prev = node;}				//Set the previous node
-
-		StationNode* getNext();										//Return the next node, if it exist
-		StationNode* getPrev();										//Return the previous node, if it exist
-		
-		bool hasNext() {return next!=nullptr;}						//Check if this node has a subsequent node
-		bool hasPrev() {return prev!=nullptr;}						//Check if this node has a precedent node
-		
-		Station* getStation() {return station;}						//Return the pointer to the station object
-		
-		class OutOfBoundsException : public std::exception {}		//Exception of the class StationNode
-		
-	private:
-		StationNode* next;											//The next node
-		StationNode* prev;											//The previous node
-		Station* station;											//The station object
-	};
-	
-	StationNode* first;															//The first station of the route
-	StationNode* last;															//The last station of the route
-	int size;																	//The total number of station in the route
-
-	std::vector<StationNode*> Iterable();										//Return a list of all the nodes for inspection
-
 public:
-	explicit StationList(int s = 0)												//Default Constructor
-	: first{nullptr}, last{nullptr}, size{s} {}
+	explicit StationList()                                          //Default Constructor
+	: first{nullptr}, last{nullptr} {}
 		
-	~StationList() {for (StationNode* index : Iterable()) delete index;}		//Destructor
+	~StationList() {for (Station* index : iterable()) delete index;}//Destructor
 
-	StationList(const StationList& list);										//Copy Constructor
-	StationList(StationList&& list);											//Move Constructor
-	StationList& operator=(const StationList& list);							//Copy Assignment
-	StationList& operator=(StationList&& list);									//Move Assignment
+	StationList(const StationList& list) = delete;                  //Copy Constructor
+	StationList& operator=(const StationList& list) = delete;       //Copy Assignment
+	StationList(StationList&& list) = default;                      //Move Constructor
+	StationList& operator=(StationList&& list) = default;           //Move Assignment
 	
-	Station* getFirst();														//Return the first station
-	Station* getLast();															//Return the last station
-	bool* isFirst(Station* station) {return station == getFirst();}				//Check if the station is the first
-	bool* isLast(Station* station) {return station == getLast();}				//Check if the station is the last
-	Station* next(Station* station);											//Return the next station, if it exist
-	Station* prev(Station* station);											//Return the previous station, if it exist
+	Station* getFirst() const;                                      //Return the first station
+	Station* getLast() const;                                       //Return the last station
 	
-	void addFirst(Station* station);											//Add a new first station
-	void addLast(Station* station);												//Add a new last station
+	void add(Station* station);                                     //Add a new station
+	std::vector<Station*> iterable() const;                         //Return a list of all the stations
 	
-	void addBefore(Station* station, Station* beforeStation);					//Add a station before another
-	void addAfter(Station* station, Station* afterStation);						//Add a station after another
-	void remove(Station* station);											//Remove a station
+	bool isEmpty() const {return first==nullptr;}                   //Check if the list is empty
 	
-	bool isEmpty();																//Check if the list is empty
+	class EmptyListException : public std::exception {};            //Exception of the class StationList
 	
-	class EmptyListException : public std::exception {}							//Exception of the class StationList
-	class InputNotFoundException : public std::exception {}					//Exception of the class StationList
+private:
+	Station* first;                                                 //The first station of the route
+	Station* last;                                                  //The last station of the route
 };
 
 #endif
