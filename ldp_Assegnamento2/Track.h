a5fb30dd4998f6c8eb22f9f94030a7c8829d1be5
Fabio R <RealPlanet@users.noreply.github.com>
2021-01-01 18:38:29 +0100

    Messi public alcun metodi che per errore erano stati messi privati

--- ldp_Assegnamento2/Track.h
+++ ldp_Assegnamento2/Track.h
@@ -5,6 +5,7 @@ class Train;
 
 class Track
 {
+public:
 	Track(TrackType _type) : m_track_type{ _type }, m_current_status{TrackStatus::FREE}{}
 	inline TrackType type() const { return m_track_type; }
 	inline TrackStatus status(){ return m_current_status; }
