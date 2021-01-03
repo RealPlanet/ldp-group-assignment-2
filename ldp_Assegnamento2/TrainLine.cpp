21aec3b99caba1c2c01925954166719d571af591
Fabio R <RealPlanet@users.noreply.github.com>
2021-01-03 23:18:02 +0100

    Aggiunte funzioni get_station_size e get_Station_distances

--- ldp_Assegnamento2/TrainLine.cpp
+++ ldp_Assegnamento2/TrainLine.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include "generics.h"
+#include "Station.h"
 
 using std::isdigit;
 using std::vector;
@@ -26,8 +27,6 @@ bool TrainLine::register_stations(std::string file_name)
 
     string newLine = "";
     
-    //Station* originStation = new Station(station_name, 1, 0);
-
     bool isOriginStation = true; //Con questo bool posso includere la prima stazione (caso degenere con un solo dato nel loop)
     while (getline(stations, newLine)) //Prendi una riga dal file
     {
@@ -35,7 +34,7 @@ bool TrainLine::register_stations(std::string file_name)
         string tempString;
         string station_name = "";
         int station_type = 1;
-        double distanza_da_origine = 0.0f;
+        int originDistance = 0;
         // Station* newStation = new Station();
         //Ora nello stringstream rimangono solo gli orari quindi itero e salvo ogni orario come intero nel vettore
         while (ss >> tempString)
@@ -47,7 +46,7 @@ bool TrainLine::register_stations(std::string file_name)
                 station_type = stoi(tempString);
                 if (ss >> tempString && is_number(tempString))
                 {
-                    distanza_da_origine = stoi(tempString);
+                    originDistance = stoi(tempString);
                 }
                 else
                 {
@@ -63,17 +62,100 @@ bool TrainLine::register_stations(std::string file_name)
         }
         if (isOriginStation)
         {
-            //Station* originalStation = new Station(station_name, station_type, distanza_da_origine);
-            //m_station_list = StationList(originalStation);
+            Station* originalStation = new MainStation(originDistance, station_name);
+            m_station_list = StationList();
+            m_station_list.add(originalStation);
+            isOriginStation = false; //Ora riprendiamo comportamento normale
         }
-
-        isOriginStation = false; //Ora riprendiamo comportamento normale
-        //Station* newStation = new Station(station_name, station_type, distanza_da_origine);
-        //m_station_list.addLast(newStation);
+        
+        Station* newStation;
+        if (station_type)
+        {
+            newStation = new MainStation(originDistance, station_name);
+        }
+        else
+        {
+            newStation = new LocalStation(originDistance, station_name);
+        }
+        
+        m_station_list.add(newStation);
     }
 
-    stations.close(); //Chiudo lo stream e rilascio le risorce occupate
+    stations.close(); //Chiudo lo stream e rilascio le risorse occupate
     return true;
+}
+
+StationInfo TrainLine::get_station_distances(int station_number, int starting_station, TrainType type) const
+{
+    /*
+    * Itera su tutte le stazioni,
+        i == numero di stazioni iterate -- j == numero di stazioni principali iterate
+
+    */
+    StationInfo info = {};
+    if (!starting_station)
+    {
+        Station* node = m_station_list.getFirst();
+        for (int i = 0, j = 0; true; i++)
+        {
+            
+            if (type == TrainType::REGIONALE && i == station_number)
+            {
+                info.m_next_station_distance = node->getDistance();
+                if(!i) info.m_prev_station_distance = node->getPrev()->getDistance(); //Se si tratta dalla stazione 1 allora non esiste distanza da quella precedente
+
+                return info;
+            }
+            else if ( (type == TrainType::ALTA_VELOCITA || type == TrainType::ALTA_VELOCITA_SUPER) && j == station_number)
+            {
+                info.m_next_station_distance = node->getDistance();
+                if (!i) info.m_prev_station_distance = node->getPrev()->getDistance(); //Se si tratta dalla stazione 1 allora non esiste distanza da quella precedente
+
+                return info;
+            }
+        
+            // Abbiamo passato una stazione principale allora incrementa j
+            if (node->getStationType() == StationType::MAIN) j++;
+
+            if (node->hasNext())
+            {
+                node = node->getNext(); //Abbiamo fatto le verifiche, proseguiamo al nodo successivo
+            }
+            else break;
+        }
+    }
+    else
+    {
+        Station* node = m_station_list.getLast();
+        for (int i = 0, j = 0; true; i++)
+        {
+            if (type == TrainType::REGIONALE && i == station_number)
+            {
+                info.m_next_station_distance = node->getPrev()->getDistance(); //Siccome percorriamo la lista al contrario dobbiamo tornare indietro per la distanza
+                if (!i) info.m_prev_station_distance = node->getNext()->getDistance(); //Se si tratta dalla stazione N allora non esiste distanza da quella successiva
+                return info;
+            }
+            else if ((type == TrainType::ALTA_VELOCITA || type == TrainType::ALTA_VELOCITA_SUPER) && j == station_number)
+            {
+                info.m_next_station_distance = node->getPrev()->getDistance(); //Siccome percorriamo la lista al contrario dobbiamo tornare indietro per la distanza
+                if (!i) info.m_prev_station_distance = node->getNext()->getDistance(); //Se si tratta dalla stazione N allora non esiste distanza da quella successiva
+                
+                return info;
+            }
+
+            // Abbiamo passato una stazione principale allora incrementa j
+            if (node->getStationType() == StationType::MAIN) j++;
+
+            //Controllo se uscire o meno qui perchè devo controllare anche l'ultimo nodo della lista
+            if (node->hasPrev())
+            {
+                node = node->getPrev(); //Abbiamo fatto le verifiche, proseguiamo al nodo successivo
+            }
+            else break;
+        }
+    }
+
+    return info; // Caso pessimo non abbiamo trovato le informazioni richieste
 }
 
 /*
--- ldp_Assegnamento2/TrainLine.h
+++ ldp_Assegnamento2/TrainLine.h
@@ -6,10 +6,11 @@ class TrainLine
 {
 public:
 	bool register_stations(std::string file_name = "");
+	int get_station_size() const { return m_station_list.size();  }
+	StationInfo get_station_distances(int station_number, int starting_station, TrainType type) const;
 
 	class InvalidStationDistance : public std::exception {};
 private:
 	StationList m_station_list;
 	bool is_number(const std::string& string) const;
 };
-
