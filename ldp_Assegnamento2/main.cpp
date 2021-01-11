10ea17eb774433f9a701f67774aa2c7aa76cf2f5
Vice <matteo.salvalaio@studenti.unipd.it>
2021-01-11 12:23:56 +0100

    Testate e compilate, manca lo stresstest

--- ldp_Assegnamento2/main.cpp
+++ ldp_Assegnamento2/main.cpp
@@ -6,43 +6,43 @@
 #include "Train.h"
 
 void run_simulation() {
-	std::cout << "Generating the train line...\n";
-	
-	TrainLine* line = new TrainLine;
+	std::cout << "[WORKER] Generating the train line...\n\n";
+	TrainLine* line = new TrainLine;                                          //Generation of the train line
 	line->register_stations("line_description.txt");
 	
-	std::cout << line->get_station_list().iterable().size() << " stations have been generated: ";
+	std::cout << "[WORKER] " << line->get_station_list().iterable().size() << " stations have been generated: ";
 	for (Station* station : line->get_station_list().iterable()) std::cout << station->getLabel();
 	std::cout << std::endl;
 	
-	TrainTime* timetable = new TrainTime;
-	timetable->register_timetable("timetables.txt",line);                     //Generations of the train line and the timetables
+	std::cout << "\n[WORKER] Generating the time tables...\n\n";
+	TrainTime* timetable = new TrainTime;                                     //Generation of the time tables
+	timetable->register_timetable("timetables.txt",line);
+	
+	std::cout << "\n[WORKER] Generating the trains...\n\n";
 	
 	std::vector<Train*> trains;
-
-	for (int key : timetable->get_timetable_trains()) {
+	for (int key : timetable->get_timetable_trains()) {                       //Generation of all the train from the timetable
 		TrainInfo info = timetable->get_train_info(key);
+
 		TrainDirection dir = TrainDirection::FORWARD;
-		if(info.n_starting_station == 1) dir = TrainDirection::BACKWARD;
-		                                                                     //Generate all the trains
+		if(info.n_starting_station != 0) dir = TrainDirection::BACKWARD;
 		if (info.m_train_type == TrainType::REGIONALE) trains.push_back(new RegionalTrain(key, dir, line, timetable));
 		else if (info.m_train_type == TrainType::ALTA_VELOCITA) trains.push_back(new HighSpeedTrain(key, dir, line, timetable));
 		else trains.push_back(new SuperHighSpeedTrain(key, dir, line, timetable));
-		
-		
-		std::cout << timetable->get_train_info(key).m_train_times.at(0) << "test ";
-		
+		std::cout << "[INFO] Train " << key << " generated\n";
 	}
 	
-	
+	std::cout << "\n[WORKER] Starting the simulation...\n\n";
 	
 	int time = 0;
-	while (trains.size() > 0 && time < 1600) {  
-								//The simulation keep running until there are still trains
+	while (trains.size() > 0) {                                              //The simulation keep running until there are still trains
+	
 		for (int index = trains.size()-1; index >= 0; index--) {
 			if (time%30==0)std::cout << "[" << (int)trains.at(index)->getDistance() << "]-" << trains.at(index)->getTrainID() << " ";
 		}
 		if (time%30==0)std::cout << "\n";
+		
+		
 		for (int index = trains.size()-1; index >= 0; index--) {
 			trains.at(index)->clock(time/60*100+time%60);                    //Call each train to update their position
 			if (trains.at(index)->isEndline()) {                             //When a train is at it's final stop destroy it
@@ -50,18 +50,19 @@ void run_simulation() {
 				trains.erase(trains.begin()+index);
 			}
 		}    
-          
-			                                                 //Call each station to update their time
-			for (Station* station : line->get_station_list().iterable()) {
-				station->clock(time/60*100+time%60); 
-			}
+		for (Station* station : line->get_station_list().iterable()) {       //Call each station to update their time
+			station->clock(time/60*100+time%60); 
+		}
 		time++;
-		
 	}
-	std::cout << trains.size();
+	delete line;
+	delete timetable;
+	std::cout << "\n[WORKER] Simulation ended...\n\n";
 }
 
 int main(int argc, char *argv[]) {
 	run_simulation();                                                        //Start the simulation
 	return 0;
 }
+
+//da testare se staz. finale è non princip.
