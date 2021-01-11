00ecd4de28380bc27075f1804aaf3ea0081bf3d5
Faldrus <arjunjassal09@gmail.com>
2021-01-12 00:33:27 +0100

    inizializzati i puntatori

--- ldp_Assegnamento2/Train.h
+++ ldp_Assegnamento2/Train.h
@@ -6,7 +6,7 @@
 #include "Station.h"
 #include "TrainTime.h"
 #include "Track.h"
-#include <tgmath.h>
+#include <ctgmath.h>
 
 static int getTrainSpeed(TrainType elem);
 
@@ -34,7 +34,7 @@ class Train{
 		Track* getTrack() {return track;}
         float getDistance() {return distance;}
         bool isEndline() {return endline;}
-        
+
         //COMUNICAZIONE CON STAZIONE
         virtual void callTrain(StationSignal si) = 0;
 
@@ -61,7 +61,7 @@ class Train{
             return trainTime->get_train_info(trainID).m_train_times.at(visitedStations);
         }
 
-    
+
     protected:
         int trainID;                                                //ID del treno
         TrainType trainType;                                        //tipo del treno
@@ -71,7 +71,7 @@ class Train{
 
         bool startline=true;                                        //controllo se è la prima stazione della linea
         bool endline=false;                                         //controllo se è l'ultima stazione della linea
-        
+
         bool parking=false;                                         //controllo se va in parcheggio o no
 
         int visitedStations;                                        //contatore delle stazioni visitate
@@ -84,11 +84,11 @@ class Train{
         int maxSpeed;                                               //velocità massima
         int currentSpeed;                                           //velocità di crociera
 
-        int time;                                                   
+        int time;
 
         float distance;                                             //distanza totale percorsa da 0
-        Station* prevStation;                                       //stazione precedente
-        Station* nextStation;                                       //stazione successiva
+        Station* prevStation=nullptr;                               //stazione precedente
+        Station* nextStation=nullptr;                               //stazione successiva
 };
 
 class RegionalTrain : public Train{
@@ -104,8 +104,8 @@ class HighSpeedTrain : public Train{
 		void callTrain(StationSignal si);
         void clock(int t);
     protected:
-        Station* prevMainStation;                                   //stazione principale precedente
-        Station* nextMainStation;                                   //stazione principale successiva    
+        Station* prevMainStation=nullptr;                                   //stazione principale precedente
+        Station* nextMainStation=nullptr;                                   //stazione principale successiva
 };
 
 class SuperHighSpeedTrain : public Train{
@@ -114,8 +114,8 @@ class SuperHighSpeedTrain : public Train{
 		void callTrain(StationSignal si);
         void clock(int t);
     protected:
-        Station* prevMainStation;                                   //stazione principale precedente
-        Station* nextMainStation;                                   //stazione principale successiva
+        Station* prevMainStation=nullptr;                                   //stazione principale precedente
+        Station* nextMainStation=nullptr;                                   //stazione principale successiva
 };
 
 static int getTrainSpeed(TrainType elem){
